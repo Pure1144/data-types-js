e127598653f7f8443a9dad1c438b1505de7c4296
Pure1144 <purevdorj@enlight.mn>
2024-12-02 16:42:31 +0800

    20241202

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Hello world")
+// data types"//
+// 1. String =>Text type=> "text"//
+// 2. Number =>Number type=> 1  6 1000//
+// 3. Boolen =>True or  false type=>false/true//
+// 4. OBJECT =>{KEY:VALUE}
+// 5. Arrey =>[index, index]
+// 6. Null  хайрцаганд байхгүй Or Undifined огт байхгүй =>no value
